0bd1b2979ff61583e733aae8cc4b93764b7901db
Esteban Patiño <gaonaestebandpg@gmail.com>
2025-03-27 14:29:55 -0500

    Acomodando sql para el uso

--- sgal_project.sql
+++ sgal_project.sql
@@ -1,31 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-03-2025 a las 20:11:53
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `sgal_project`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ciclo_cultivo`
---
+CREATE DATABASE IF NOT EXISTS sgal_project;
+USE sgal_project;
 
 CREATE TABLE `ciclo_cultivo` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -38,12 +12,6 @@ CREATE TABLE `ciclo_cultivo` (
   `descripcion` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cultivo`
---
 
 CREATE TABLE `cultivo` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -58,11 +26,6 @@ CREATE TABLE `cultivo` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `insumo`
---
 
 CREATE TABLE `insumo` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -78,12 +41,6 @@ CREATE TABLE `insumo` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `sensor`
---
-
 CREATE TABLE `sensor` (
   `id` int(10) UNSIGNED NOT NULL,
   `nombre` varchar(25) NOT NULL,
@@ -94,12 +51,6 @@ CREATE TABLE `sensor` (
   `descripcion` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
 
 CREATE TABLE `usuario` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -114,79 +65,35 @@ CREATE TABLE `usuario` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `ciclo_cultivo`
---
 ALTER TABLE `ciclo_cultivo`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `cultivo`
---
 ALTER TABLE `cultivo`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `identificador` (`identificador`);
 
---
--- Indices de la tabla `insumo`
---
 ALTER TABLE `insumo`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `sensor`
---
 ALTER TABLE `sensor`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `usuario`
---
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `num_documento` (`num_documento`),
   ADD UNIQUE KEY `correo` (`correo`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `ciclo_cultivo`
---
 ALTER TABLE `ciclo_cultivo`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `cultivo`
---
 ALTER TABLE `cultivo`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `insumo`
---
 ALTER TABLE `insumo`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `sensor`
---
 ALTER TABLE `sensor`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `usuario`
---
 ALTER TABLE `usuario`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-  
