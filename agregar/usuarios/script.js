29140c7d04499811f0ee63632dca1aefd1b5a19e
isabella-lopez02 <isabellalopezarango@gmail.com>
2025-03-31 15:35:20 -0500

    new form users

---
+++ agregar/usuarios/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.form');
+
+    form.addEventListener('submit', async(event) =>{
+        event.preventDefault();
+
+        const userName = document.querySelector('.register__input--name');
+        const userEmail = document.querySelector('.register__input--email');
+        const userAdress = document.querySelector('.register__input--address');
+        const userCellphone = document.querySelector('.register__input--cellphone');
+
+        const formdata = {
+            // Evita erroes si algún campo no existe userName ?
+            name: userName ? userName.value : '',
+            email: userEmail ? userEmail.value : '',
+            address: userAdress ? userAdress.value : null,
+            phone: userCellphone ? userCellphone.value : ''
+        };
+
+        try{
+            const response = await fetch('http://localhost:3000/usuario', {
+                method: 'POST',
+                // Se está indicando que el cuerpo 
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                // Petición fetch para enviar datos en formato json a el servidor.
+                body: JSON.stringify(formdata)
+            });
+            
+            if(!response.ok){
+                throw new Error('Error en la conexión del servidor');
+            }
+            
+            const result = await response.json();
+            console.log('Usuario registrado', result);
+
+            form.reset(); // Limpiar formulario
+
+        } catch(error){
+            console.error('Error', error);
+        }
+    });
+})
+
